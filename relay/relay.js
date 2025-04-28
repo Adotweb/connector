c5343e7c797e399345d60d15c86c5d8ac38836d8
Alim Weber <alim.weber@outlook.com>
2025-04-28 19:57:04 +0200

    relay comments

--- relay/relay.js
+++ relay/relay.js
@@ -1,67 +1,68 @@
 const { WebSocket } = require("ws");
 const { ipcMain } = require("electron");
 const { services } = require("../services/run_services");
-
 const { uniqueNamesGenerator, adjectives, colors, animals} = require("unique-names-generator")
 
+
+//we generate a random hostname that is memorable (something like fast_tiger)
 const host_id = uniqueNamesGenerator({
 	dictionaries : [adjectives, colors, animals],
 	length : 2
 });
 
-
+//this registers the "get_host_id" function so we can use it in the frontend
 ipcMain.handle("get_host_id", async () => {
 	return host_id
 })
 
 function start_ws(){
 
-let ws = new WebSocket("https://connector.neptunapp.org/");
+	//we first connect a new websocket to the relay server (written and documented in connector_hub)
+	let ws = new WebSocket("https://connector.neptunapp.org/");
 
 
+	//as soon as we opened we send a host-login event to notify the relay that a new host is loggin in
+	ws.onopen = () => {	
+		ws.send(JSON.stringify({
+			sender : "host",
+			method : "login",
+			body : {
+				host_id,
+			}
+		}))
+	}
 
-
-ws.onopen = () => {	
-	ws.send(JSON.stringify({
-		sender : "host",
-		method : "login",
-		body : {
-			host_id,
-		}
-
-	}))
-}
-
-ws.onmessage = proto => {
-	let message = JSON.parse(proto.data)
+	//whenever we get a message we process the method, 
+	//currently only very simple rest requests work (i.e. HTML page requests with GET and in specific cases POST)
+	ws.onmessage = proto => {
+		let message = JSON.parse(proto.data)
 	
+		switch(message.method){
+			case "rest-request" : handle_rest(ws, message, message.meta.request_id)		
+				break;
+		}	
+	
+	}
 
-	switch(message.method){
-		case "rest-request" : handle_rest(ws, message, message.meta.request_id)		
-			break;
-	}	
+	//we send a keepalive signal to the relay every 3 seconds to keep the connection alive
+	setInterval(() => {
+		ws.send(JSON.stringify({
+			sender : "host",
+			method : "keepalive"
+		}))	
+	}, 3_000)
 
 }
 
 
-setInterval(() => {
-	ws.send(JSON.stringify({
-		sender : "host",
-		method : "keepalive"
-	}))	
-}, 3_000)
-
-}
-
+//this function is mostly copied from chatgpt annotataed with some lines for better adaptation in the project
 async function fetchFromRequest(req, baseUrl = '') {
   // Reconstruct full URL
 
-
-	//selbst geschrieben
+	//split the service name because the url will be something like /service_name/sub_path, so we can get the service_name
 	let service_name = req.url.split("/")[1];
 
-
-
+	//get the inward path of the service so we know where we have to fetch on localhost
 	let inward_port = services.get(service_name).inward_port;
 
   const url = `http://localhost:${inward_port}`;
@@ -93,9 +94,10 @@ async function fetchFromRequest(req, baseUrl = '') {
     headers,
     body,
   })
-	
+
+
 	let ret;
-	// Check the content type
+	// Check the content type, and return the output of the given method
 	const contentType = response.headers.get('content-type');
 
 	if (contentType.includes('application/json')) {
@@ -113,14 +115,15 @@ async function fetchFromRequest(req, baseUrl = '') {
 
 }
 
+
+//intermediate function that handles 
 async function handle_rest(socket, message, request_id){
 
 	let body = message.body;
-
-
+	//we compute the rest response
 	let response = await fetchFromRequest(body);
 
-
+	//then we send, the response in the right format
 	socket.send(JSON.stringify({
 		sender : "host",
 		method : "rest-response",
