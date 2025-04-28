46a44da2d6c83992e1c727e84baad7f8efc82a3c
Alim Weber <alim.weber@outlook.com>
2025-04-28 20:02:05 +0200

    run_services comments

--- services/run_services.js
+++ services/run_services.js
@@ -9,10 +9,13 @@ const { app } = require('electron');
 //a map to store services (things that hold information about processes)
 const services = new Map()
 
+//we try to get the services the user has already defined from the path they are stored
+//we do this in a try block so that it cannot throw an error
 try {
 	let userDataPath = app.getPath("userData");
 	let file = JSON.parse(fs.readFileSync(path.join(userDataPath, "services", "services.json"), "utf8"))
-	
+
+	//then we set a Map reference for every JSON object in the file
 	file.forEach(service => {
 		services.set(service.service_name, service)
 	})
@@ -25,61 +28,64 @@ try {
 const processes = new Map();
 
 // Start a process
+// in part copied by chatgpt (everything that has less indents)
 function startProcess(service) {
 	let {run_command, path, service_name : name, env} = service	
-	
+
+
+	//this actually runs the command, we provide the path where the repo we installed is at, the command we run
+	//and the environment variables
 	const proc = spawn(run_command, {cwd : path, stdio: 'pipe', shell : true , env : {
 		...process.env,
 		...env
 	}});
 
-
+	//we set the services running state to true to reflect in the fronted
 	service.running = true;
 
 
-  // Log output to console
+	//write stdout to the logs attribute so we can show in frontend
   proc.stdout.on('data', (data) => {
-
 	  services.get(name).logs += `\n[${new Date().toUTCString()}] ${data}`
   });
 
+	//same with errors
   proc.stderr.on('data', (data) => {
 	  services.get(name).logs += `\n[${new Date().toUTCString()}, error] ${data}`
   });
 
+	//once we close we perform some cleanup, set the running state to false and delete from process map
   proc.on('close', (code) => {
 
-	  if(services.has(name)){
-	  	services.get(name).running = false
-	  }
+		if(services.has(name)){
+	  		services.get(name).running = false
+	  	}
 
-    processes.delete(name); // Clean up if needed
+    		processes.delete(name); 
   });
 
 
 	
 
-  // Store the process
-  processes.set(name, proc);
+  	//store the process with the name, and store the service (that has the meta information)
+  	processes.set(name, proc);
 	services.set(name, service)
 
 }
 
-// Kill a process
+//stops a process?!?!
 function stopProcess(name) {
-  const proc = processes.get(name);
-	console.log(proc.pid, name, services.get(name))
+	//get the process 
+  	const proc = processes.get(name);
 
-  if (proc) {
-	  services.get(name).running = false
-	
-	  kill(proc.pid)
-
-    processes.delete(name);
-    console.log(`[${name}] process killed`);
-  } else {
-    console.log(`[${name}] process not found`);
-  }
+	//if the process exists we set its running state to false and kill it with its PID
+  	if (proc) {
+	  	services.get(name).running = false	
+	  	kill(proc.pid)
+    		processes.delete(name);
+  	} else {
+    		console.log(`[${name}] process not found`);
+  	}
 }
 
 module.exports = {
